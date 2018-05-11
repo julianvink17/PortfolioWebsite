cd04881dd2f0fda2222fab570f20c2bfbb8a2a8b
Julianvink17 <julian-_-vink@hotmail.com>
2018-05-11 16:41:19 +0200

    eerste commit

---
+++ message1.php
@@ -0,0 +1,13 @@
+<?php
+$name   = $_POST["name"];
+$email      = $_POST["email"];
+$message   = $_POST["message"];
+require_once "connect.php";
+$sql = $conn->prepare("INSERT INTO message VALUES(:name, :email, :message)");
+$sql->execute([
+    "name" => $name,
+    "email"     => $email,
+    "message"     => $message,
+]);
+header("Location: http://localhost/Portfolio2/index.html");
+?>
---
+++ read.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>read.php</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<?php
+require_once "connect.php";
+$sql = $conn->prepare("select name, email, message from message");
+$sql->execute();
+echo "<div class='terug'>";
+echo "<a href='index.html'> Terug naar het portfolio </a>";
+echo "</div>";
+echo "<table>";
+echo "<tr style= 'border-bottom: solid 1px'>";
+echo "<th style='background-color: #6071f3'>Naam</th>";
+echo "<th style='background-color: #6071f3'>E-mail</th>";
+echo "<th style='background-color: #6071f3'>Bericht</th>";
+
+echo "</tr>";
+foreach ($sql as $rij) {
+    echo "<tr style='text-align: center'>";
+    echo "<td style='background-color: #ccf6e7'>" . $rij["name"] . "</td>";
+    echo "<td style='background-color: #ccf6e7'>" . $rij["email"] . "</td>";
+    echo "<td style='background-color: #ccf6e7'>" . $rij["message"] . "</td>";
+    echo "</tr>";
+}
+echo "</table><br/>";
+
+?>
+</body>
+</html>
